620f017fde957324a1c9b96dd84d3dc2ec4dc1f4
Jonir Rings <i@jonirrings.com>
2025-04-18 08:39:43 +0800

    try to deploy

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+import {LayoutProps} from "@/app/lib/definitions";
+
+export default function Layout(props: LayoutProps) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav/>
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {props.children}
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import {inter} from "@/app/ui/fonts";
+import {LayoutProps} from "@/app/lib/definitions";
+
+
+export default function RootLayout(props: LayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+    <body className={`${inter.className} antialiased`}>{props.children}</body>
     </html>
   );
 }
